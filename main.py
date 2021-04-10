218fb1241ef835dd7988242a778a1b51e9ecae67
winnerdinner701 <christhegod701@gmail.com>
2021-04-10 07:28:29 +0100

    h

--- main.py
+++ main.py
@@ -13,12 +13,23 @@ def buber():
     return render_template('buber.html')
 
 @app.route('/admin/<n>/')
-def admin(n):
+def admin2(n):
     if n in a:
         return redirect(url_for('admin_link', u='idf9ujer9tuswdiw9peowf0h9uf0iwef9h80'+n+'sjieujiugrjfeowihhgwoif9ghfwuhfw8f', n=n))
     else:
         return redirect(url_for("permission_admin", n='admin'))
-        
+
+@app.route("/admin/", methods=["GET", "POST"])
+def admin1():
+    if request.method == "POST":
+        req = request.form
+        print(req)
+        name = request.form.get("name")
+        if name in a:
+            return redirect(url_for('admin_link', n=name, u='idf9ujer9tuswdiw9peowf0h9uf0iwef9h80'+name+'sjieujiugrjfeowihhgwoif9ghfwuhfw8f'))
+        else:
+            return redirect(url_for('permission_admin', n='admin'))
+    return render_template('admin.html')
 
 @app.route('/admin/<u>/user/<n>')
 def admin_link(u, n):
