9ac2b99edc87972d175f7e885dea3c3b8f5f40db
winnerdinner701 <christhegod701@gmail.com>
2021-04-10 13:20:08 +0100

    j

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ def dad():
 @app.route('/permission_denied/<n>/<u>/<p>/')
 def permission_admin(n, u, p):
     return render_template('permission.html', n=n, u=u, p=p)
+    return redirect(url_for('home'))
 
 @app.route('/pages/')
 def pages():
